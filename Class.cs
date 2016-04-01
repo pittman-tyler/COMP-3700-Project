44dc721840490540ce67cb1da73324689c8aa1e4
pittman-tyler <pittman.tyler@live.com>
2016-03-31 19:41:48 -0500

    Basic Classes
    This is just the constructors and fields for the classes we have so far.
    I'll add methods by the end of the weekend.

---
+++ Class.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Course
+{
+	public Course(int course_id_input, string name_input, string description_input, int teacher_id_input, int credit_hours_input, int max_size_input)
+	{
+        int course_id = course_id_input;
+        string name = name_input;
+        string description = description_input;
+        int teacher_id = teacher_id_input;
+        int credit_hours = credit_hours_input;
+        int max_size = max_size_input;
+        int current_size = 0;
+	}
+}
---
+++ User.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+public class User
+{
+	public User(string name_in, int user_id_in, string username_in, string password_in)
+	{
+        string name = username_in;
+        int user_id = user_id_in;
+        string username = username_in;
+        string password = password_in;
+	}
+}
+
+public class Student : User
+{
+    public Student(string name_in, int user_id_in, string username_in, string password_in)
+    {
+        User(name_in, user_id_in, username_in, password_in);
+        bool registration_status = false;
+        bool withdraw = false;
+    }
+}
+
+public class Teacher : User
+{
+    public Teacher(string name_in, int user_id_in, string username_in, string password_in)
+    {
+        User(name_in, user_id_in, username_in, password_in);
+    }
+}
+
+public class Administrator : User
+{
+    public Administrator(string name_in, int user_id_in, string username_in, string password_in)
+    {
+        User(name_in, user_id_in, username_in, password_in);
+    }
+}
